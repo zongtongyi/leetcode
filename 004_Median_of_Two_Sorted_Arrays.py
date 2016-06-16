6f5e7e736fdbdc90f213f1fceb2b7bef956566ea
Peng Wang <wangpeng@zmeng123.com>
2016-06-16 15:41:14 +0800

    Submit and Accept -- 004 Median of Two Sorted Arrays. refer jiuzhang.com

--- 004_Median_of_Two_Sorted_Arrays.py
+++ 004_Median_of_Two_Sorted_Arrays.py
@@ -10,20 +10,33 @@ class Solution(object):
         """
         n = len(nums1) + len(nums2)
         if n%2 == 1:
-            return findKth(nums1, nums2, n/2+1)
+            return self.findKth(nums1, nums2, n/2+1)
         else:
-            k1 = findKth(nums1, nums2, n/2)
-            k2 = findKth(nums1, nums2, n/2+1)
+            k1 = self.findKth(nums1, nums2, n/2)
+            k2 = self.findKth(nums1, nums2, n/2+1)
             return (k1+k2)/2.0
 
-    def findKth():
-        pass
+    def findKth(self, nums1, nums2, k):
+        if nums1==[]:
+            return nums2[k-1]
+        if nums2==[]:
+            return nums1[k-1]
+        if k==1:
+            return min(nums1[0], nums2[0])
+        a = nums1[k/2 - 1] if len(nums1)>=k/2 else None
+        b = nums2[k/2 - 1] if len(nums2)>=k/2 else None
+        
+        if b is None or (a is not None and a<b):
+            return self.findKth(nums1[k/2:], nums2, k-k/2)
+        else:
+            return self.findKth(nums1, nums2[k/2:], k-k/2)
 
 
 if __name__ == "__main__":
-    # l1, l2 = [], [1]
+    l1, l2 = [], [1]
     # l1, l2 = [1], [1]
-    l1, l2 = [1, 2, 3, 4], [5, 6, 7]
+    # l1, l2 = [1, 2, 3, 4], [5, 6, 7]
     # l1, l2 = [1, 2, 8, 10], [5, 6, 7]
     # l1, l2 = [1, 2, 4, 8], [5, 6, 8, 10]
+    l1, l2 = [1], [2, 3, 4, 5, 6]
     print Solution().findMedianSortedArrays(l1, l2)
