0ba981280b6a262e5b5a37ebcf4280859e5117f4
Peng Wang <wangpeng@zmeng123.com>
2016-06-28 10:48:03 +0800

    Submit and Accept -- 007 Reverse Integer

--- 007_Reverse_Integer.py
+++ 007_Reverse_Integer.py
@@ -7,24 +7,41 @@ INT_MIN = -2147483648
 
 class Solution(object):
     def reverse(self, x):
-        sig, res = 1, 0
+        sig = 1
         if x < 0:
-            sig = -1
+            sig, x = -1, abs(x)
 
-        while x>0:
+        res = 0
+        while x > 0:
             res = res*10 + x%10
             x /= 10
 
-        return sig * res
+        res *= sig
+        if res > 2**31 - 1 or res < -2**31:
+            return 0
+
+        return res
 
 class Solution(object):
     def reverse(self, x):
+        sig = 1
+        if x < 0:
+            sig, x = -1, abs(x)
+
         sx = str(x)
         sx = sx[::-1]
         ix = int(sx)
 
+        ix *= sig
+        if ix > 2**31 - 1 or ix < -2**31:
+            return 0
+
+        return ix
+
+
 if __name__ == "__main__":
-    Solution().reverse(123)
-    Solution().reverse(-123)
-    Solution().reverse(100)
-    Solution().reverse(1000000003)
+    print Solution().reverse(123)
+    print Solution().reverse(-123)
+    print Solution().reverse(100)
+    print Solution().reverse(1020)
+    print Solution().reverse(1000000003)
