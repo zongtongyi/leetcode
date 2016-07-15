b1d8414b0b8b0a28d583daa1d29524299de391bc
Peng Wang <wangpeng@zmeng123.com>
2016-07-15 20:42:07 +0800

    Submit and Accept -- 036 Valid Sudoku

--- 036_Valid_Sudoku.py
+++ 036_Valid_Sudoku.py
@@ -3,7 +3,27 @@
 
 class Solution(object):
     def isValidSudoku(self, board):
-        pass
+        rows = [[] for i in range(9)]
+        colm = [[] for i in range(9)]
+        subboard = [[] for i in range(9)]
+        for i in range(9):
+            for j in range(9):
+                x = board[i][j]
+                if x == '.':
+                    continue
+
+                if x in rows[i]:
+                    return False
+                if x in colm[j]:
+                    return False
+                rows[i].append(x)
+                colm[j].append(x)
+
+                si = i/3*3 + j/3
+                if x in subboard[si]:
+                    return False
+                subboard[si].append(x)
+        return True
 
 
 # Solution 2: bitmap
