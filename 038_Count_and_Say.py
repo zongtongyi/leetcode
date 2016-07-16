fe505de76de5633dbc3356b57e18ffd30e39a940
Peng Wang <wangpeng@zmeng123.com>
2016-07-16 19:55:40 +0800

    Submit and Accept -- 038 Count and Say

--- 038_Count_and_Say.py
+++ 038_Count_and_Say.py
@@ -2,11 +2,27 @@
 # -*- coding:utf-8 -*-
 
 class Solution(object):
-    def countAndSay(self, n):
-        pass
+    def count(self, s):
+        ss, sn, sout = s[0], 1, ''
+        for x in xrange(1, len(s)):
+            if s[x]==ss:
+                sn += 1
+            else:
+                sout = sout + str(sn) + ss
+                ss = s[x]
+                sn = 1
 
+        return sout + str(sn) + ss
+
+    def countAndSay(self, n):
+        s = '1'
+        for x in xrange(2, n+1):
+            s = self.count(s)
+        return s
 
 
 if __name__ == "__main__":
     n = 1211
+    n = 11
+    # n = 1
     print Solution().countAndSay(n)
