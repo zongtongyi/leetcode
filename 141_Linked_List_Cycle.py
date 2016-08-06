01f923348b36fac858a68ca34b9ae378bc00c83e
Peng Wang <wangpeng@zmeng123.com>
2016-08-06 17:23:54 +0800

    Submit and Accept -- 141 Linked List Cycle

---
+++ 141_Linked_List_Cycle.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+def printlist(head):
+    print head.val
+    while head.next:
+        head = head.next
+        print head.val
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if not head or not head.next: return False
+
+        p1, p2 = head, head.next.next
+        while p1:
+            if p1 == p2: return True
+
+            try:
+                p1, p2 = p1.next, p2.next.next
+            except AttributeError as e:
+                return False
+
+        return False
+
+
+if __name__ == "__main__":
+    node1 = ListNode(1)
+    node2 = ListNode(6)
+    node3 = ListNode(3)
+    node1.next = node2
+    # node2.next = node1
+
+    print Solution().hasCycle(node1)
