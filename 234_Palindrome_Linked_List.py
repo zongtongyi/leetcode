03e5d74e591cb3954acd97ee794be18f8a84e9a5
Peng Wang <wangpeng@zmeng123.com>
2016-08-07 19:01:05 +0800

    Submit and Accept -- 234 Palindrome Linked List

--- 234_Palindrome_Linked_List.py
+++ 234_Palindrome_Linked_List.py
@@ -1,12 +1,13 @@
 #! /usr/bin/env python
 # -*- coding:utf-8 -*-
+# leetcode 234 Palindrome
 
 def printlist(head):
     print head.val
     while head.next:
         head = head.next
         print head.val
-
+        
 # Definition for singly-linked list.
 class ListNode(object):
     def __init__(self, x):
@@ -15,17 +16,38 @@ class ListNode(object):
 
 class Solution(object):
     def isPalindrome(self, head):
-        """
-        :type head: ListNode
-        :rtype: bool
-        """
+        if not head: return True
+
+        slow, fast = head, head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        if fast:
+            slow = slow.next
+
+        last = None
+        while slow:
+            tmp = slow.next
+            slow.next = last
+            last = slow
+            slow = tmp
+
+        p = head
+        while last:
+            if p.val == last.val:
+                p, last = p.next, last.next
+            else:
+                return False
+
+        return True
+
 
 if __name__ == "__main__":
     l1 = ListNode(1)
-    l1.next = ListNode(6)
+    l1.next = ListNode(2)
+    #l1.next.next = ListNode(3)
     l1.next.next = ListNode(1)
-    l1.next.next.next = ListNode(3)
-    l1.next.next.next.next = ListNode(11)
-    l1.next.next.next.next.next = ListNode(4)
+    # l1.next.next.next = ListNode(1)
 
-    print Solution.isPalindrome(head)
+    print Solution().isPalindrome(l1)
