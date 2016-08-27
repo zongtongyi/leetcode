882b20897019286d8b3a6fab7bc14f5988000688
Peng Wang <wangpeng@zmeng123.com>
2016-08-27 11:36:10 +0800

    initial 328_Odd_Even_Linked_List.py

---
+++ 328_Odd_Even_Linked_List.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+def prl(head):
+    if not head: print head
+    while head:
+        print head.val
+        head = head.next
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def oddEvenList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
