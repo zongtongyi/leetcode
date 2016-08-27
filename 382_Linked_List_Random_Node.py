d4f998262c1bbf0e97898d7cd41d8025bf15cacb
Peng Wang <wangpeng@zmeng123.com>
2016-08-27 11:34:46 +0800

    initial 382_Linked_List_Random_Node.py

---
+++ 382_Linked_List_Random_Node.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+def prl(head):
+    if not head: print head
+    while head:
+        print head.val
+        head = head.next
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def __init__(self, head):
+        """
+        @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node.
+        :type head: ListNode
+        """
+    
+    def getRandom(self):
+        """
+        Returns a random node's value.
+        :rtype: int
+        """
+
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(head)
+# param_1 = obj.getRandom()
