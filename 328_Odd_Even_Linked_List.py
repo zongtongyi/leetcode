d2435ac0aee69b7a7495e3d8678cdb62b9aa3b21
Peng Wang <wangpeng@zmeng123.com>
2016-08-29 23:34:09 +0800

    Submit and Accept -- 328 Odd Even Linked List, with three different implementation

--- 328_Odd_Even_Linked_List.py
+++ 328_Odd_Even_Linked_List.py
@@ -13,8 +13,59 @@ class ListNode(object):
         self.next = None
 
 class Solution(object):
+    '''
     def oddEvenList(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
+        # if not head or not head.next: return head
+        if not head: return head
+
+        odd_head, even_head = head, head.next
+        odd, even = odd_head, even_head
+        while even and even.next:
+            odd.next = even.next
+            odd = odd.next
+            even.next = odd.next
+            even = even.next
+
+        odd.next = even_head
+        return odd_head
+    '''
+    '''
+    def oddEvenList(self, head):
+        if not head or not head.next: return head
+
+        odd, even = head, head.next
+        while even and even.next:
+            tmp = odd.next
+            odd.next = even.next
+            odd = odd.next
+            even.next = odd.next
+            odd.next = tmp
+            even = even.next
+
+        return head
+    '''
+    def oddEvenList(self, head):
+        if not head: return head
+
+        odd, even = head, head.next
+        while even and even.next:
+            next_odd, next_even = odd.next, even.next
+            even.next = next_even.next
+            odd.next, next_even.next = next_even, next_odd
+            odd, even = odd.next, even.next
+
+        return head
+
+
+
+if __name__ == "__main__":
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    head.next.next.next.next = ListNode(5)
+    head.next.next.next.next.next = ListNode(6)
+
+    l2 = Solution().oddEvenList(head)
+    prl(l2)
+
