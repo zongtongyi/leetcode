ff32adce51e2bbe702dc34a63e92f5df10d758a5
Peng Wang <wangpeng@zmeng123.com>
2016-08-30 12:27:03 +0800

    workable solution for 25 Reverse Nodes in k-Group

--- 25_Reverse_Nodes_in_k-Group.py
+++ 25_Reverse_Nodes_in_k-Group.py
@@ -20,16 +20,50 @@ class Solution(object):
         :type k: int
         :rtype: ListNode
         """
+        if not head or not head.next: return head
+
+        bHead = False
+        pre = ListNode(0)
+        pre.next, first = head, head
+        while True:
+            i = k
+            p1, p2 = first, first
+            while i>1:
+                p2 = p2.next
+                if not p2: return head
+                i -= 1
+
+            last = p2.next
+            first = last
+            while p1 != p2:
+                tmp = p1.next
+                p1.next = last
+                last = p1
+                p1 = tmp
+            p2.next = last
+            pre.next = p2
+
+            if not bHead:
+                head = p2
+                bHead = True
+
+            i = k
+            while i>0:
+                pre = pre.next
+                i -= 1
+
+
+
 
 
 if __name__ == "__main__":
-    a = ListNode(3)
-    b = ListNode(8)
-    c = ListNode(6)
-    d = ListNode(1)
-    e = ListNode(2)
+    a = ListNode(1)
+    b = ListNode(2)
+    c = ListNode(3)
+    d = ListNode(4)
+    e = ListNode(5)
     a.next, b.next, c.next, d.next = b, c, d, e
 
-    l2 = Solution().reverseKGroup(a, k)
+    l2 = Solution().reverseKGroup(a, 2)
     prl(l2)
 
